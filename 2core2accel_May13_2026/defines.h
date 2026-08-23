3f62364684e740277bef96364afe85fa1f37f6fa
Jake Peyser <jacob.peyser@gmail.com>
2026-08-23 15:51:53 -0400

    Update defines.h

--- 2core2accel_May13_2026/defines.h
+++ 2core2accel_May13_2026/defines.h
@@ -9,5 +9,6 @@
 
 char ssid[] = "cuWaterRouter";        // your network SSID (name)
 char pass[] = "mrmoop2026!";         // your network password (use for WPA, or use as key for WEP), length must be 8+
+                                    // edit: 8/23/26: This password should technically be redacted, but if came all this way just for it, you deserve it. 
 
 #endif    //defines_h
